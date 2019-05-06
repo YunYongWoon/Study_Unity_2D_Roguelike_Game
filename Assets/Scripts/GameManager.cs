8a0519ac55443324afb40e74c7de86e33299cfc4
Yun_Yongwoon <yuyun0124@gmail.com>
2019-05-06 20:29:50 +0900

    12. Adding UI&Level Transitions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour{
+	public float levelStartDelay = 2f;
 	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScropt;
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool playersTurn = true;
 
-	private int level = 3;
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
 	private List<Enemy> enemies;
 	private bool enemiesMoving;
+	private bool doingSetup;
 
 	// Start is called before the first frame update
     void Awake() {
@@ -26,19 +31,39 @@ public class GameManager : MonoBehaviour{
 		InitGame();
     }
 
+	private void OnLevelWasLoaded (int indel) {
+		level++;
+		InitGame();
+	}
+
 	void InitGame () {
+		doingSetup = true;
+
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+		levelText.text = "Day " + level;
+		levelImage.SetActive(true);
+		Invoke("HideLevelImage", levelStartDelay);
+
 		enemies.Clear();
 		boardScropt.SetupScene(level);
 	}
 
+	public void HideLevelImage () {
+		levelImage.SetActive(false);
+		doingSetup = false;
+	}
+
 	public void GameOver () {
+		levelText.text = "After " + level + " days, you died.";
+		levelImage.SetActive(true);
 		enabled = false;
 	}
 
     // Update is called once per frame
     void Update()
     {
-		if (playersTurn || enemiesMoving)
+		if (playersTurn || enemiesMoving || doingSetup)
 			return;
 
 		StartCoroutine(MoveEnemies());
