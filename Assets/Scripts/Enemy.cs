54c6e63ae9f358ad8954edce3a1e53ff2471081c
Yun_Yongwoon <yuyun0124@gmail.com>
2019-05-06 18:58:51 +0900

    11. Enemy Animator Controller_MissingReferenceException

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,16 +14,13 @@ public class Enemy : MovingObject {
     // Start is called before the first frame update
     protected override void Start()
     {
+		GameManager.instance.AddEnemyToList(this);
 		animator = GetComponent<Animator>();
 		target = GameObject.FindGameObjectWithTag("Player").transform;
 		base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Update is called once#    }
 
 	protected override void AttemptMove<T> (int xDir, int yDir) {
 		if (skipMove) {
@@ -38,17 +35,19 @@ public class Enemy : MovingObject {
 	public void MoveEnemy() {
 		int xDir = 0;
 		int yDir = 0;
-
 		if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
 			yDir = target.position.y > transform.position.y ? 1 : -1;
 		else
 			xDir = target.position.x > transform.position.x ? 1 : -1;
-
+			
 		AttemptMove<Player>(xDir, yDir);
 	}
 
 	protected override void OnCantMove<T> (T component) {
 		Player hitPlayer = component as Player;
+
 		hitPlayer.LoseFood(playerDamage);
+
+		animator.SetTrigger("EnemyAttack");
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour{
+	public float turnDelay = .1f;
 	public static GameManager instance = null;
 	public BoardManager boardScropt;
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool playersTurn = true;
 
 	private int level = 3;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
 
 	// Start is called before the first frame update
     void Awake() {
@@ -16,11 +19,15 @@ public class GameManager : MonoBehaviour{
 			instance = this;
 		else if (instance != this)
 			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+		enemies = new List<Enemy>();
 		boardScropt = GetComponent<BoardManager>();
 		InitGame();
     }
 
 	void InitGame () {
+		enemies.Clear();
 		boardScropt.SetupScene(level);
 	}
 
@@ -31,6 +38,30 @@ public class GameManager : MonoBehaviour{
     // Update is called once per frame
     void Update()
     {
-        
+		if (playersTurn || enemiesMoving)
+			return;
+
+		StartCoroutine(MoveEnemies());
     }
+
+	public void AddEnemyToList (Enemy script) {
+		enemies.Add(script);
+	}
+
+	IEnumerator MoveEnemies () {
+		enemiesMoving = true;
+
+		yield return new WaitForSeconds(turnDelay);
+
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds(turnDelay);
+		}
+		for (int i = 0; i < enemies.Count; i++) {
+			enemies[i].MoveEnemy();
+			yield return new WaitForSeconds(enemies[i].movetime);
+		}
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+
 }
